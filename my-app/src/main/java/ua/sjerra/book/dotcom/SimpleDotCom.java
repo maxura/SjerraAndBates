a3b708d0d3b3013fef9998f8cabb95af6e70c925
Maxim Musienko <mmusienko@codenvy.com>
2015-03-09 01:08:03 +0200

    sd

--- my-app/src/main/java/ua/sjerra/book/dotcom/SimpleDotCom.java
+++ my-app/src/main/java/ua/sjerra/book/dotcom/SimpleDotCom.java
@@ -4,7 +4,23 @@ package ua.sjerra.book.dotcom;
  * Created by mmusienko on 2/23/15.
  */
 public class SimpleDotCom {
+
     int[] locationCells;
+
+    public static void main(String[] args) {
+        int numOfGuess = 0;
+        GameHelper helper = new GameHelper();
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+        while (isAlive == true) {
+            String guess = helper.getUserInput("Enter your number");
+        }
+    }
+
+
     int numOfHits = 0;
 
     void setLocationCells(int[] locs) {
