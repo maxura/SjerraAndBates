ff3e86d996a40fe1cb649a99b3f9a9f6a8a631cc
Maxim Musienko <mmusienko@codenvy.com>
2015-04-06 19:12:42 +0300

    sd

---
+++ my-app/src/main/java/ua/sjerra/book/objectivile/Cat.java
@@ -0,0 +1,9 @@
+package ua.sjerra.book.objectivile;
+
+import com.sun.prism.shader.FillEllipse_LinearGradient_PAD_Loader;
+
+/**
+ * Created by mmusienko on 4/6/15.
+ */
+public class Cat extends Feline {
+}
--- my-app/src/main/java/ua/sjerra/book/objectivile/MyDogLis.java
+++
@@ -1,8 +0,0 @@
-package ua.sjerra.book.objectivile;
-
-/**
- * Created by mmusienko on 4/3/15.
- */
-public class MyDogLis {
-
-}
