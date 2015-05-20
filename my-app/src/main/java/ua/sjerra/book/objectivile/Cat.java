dc6ad212a5421554a36db06203e22281ebca9ebc
maxura <maxura@ukr.net>
2015-05-20 22:02:25 +0300

    do tasks

--- my-app/src/main/java/ua/sjerra/book/objectivile/Cat.java
+++ my-app/src/main/java/ua/sjerra/book/objectivile/Cat.java
@@ -1,6 +1,5 @@
 package ua.sjerra.book.objectivile;
 
-import com.sun.prism.shader.FillEllipse_LinearGradient_PAD_Loader;
 
 /**
  * Created by mmusienko on 4/6/15.
--- my-app/src/main/java/ua/sjerra/book/objectivile/task/Clowns.java
+++ my-app/src/main/java/ua/sjerra/book/objectivile/task/Clowns.java
@@ -3,5 +3,6 @@ package ua.sjerra.book.objectivile.task;
 /**
  * Created by mmusienko on 20.05.15.
  */
-public class Clown {
+public class Clowns extends  Picasso{
+
 }
