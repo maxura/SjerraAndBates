9bf5dc5a94f47dc8bab644c1e85adc6054d36fc9
maxura <maxura@ukr.net>
2015-06-09 22:42:53 +0300

    add new samples

---
+++ my-app/src/main/java/ua/sjerra/book/numbers/StaticSuper.java
@@ -0,0 +1,13 @@
+package ua.sjerra.book.numbers;
+
+/**
+ * Created by Natalya on 01.06.2015.
+ */
+public class StaticSuper {
+    static {
+        System.out.println("Parent static block");
+    }
+    StaticSuper(){
+        System.out.println("Parent constructor");
+    }
+}
---
+++ my-app/src/main/java/ua/sjerra/book/numbers/StaticTest.java
@@ -0,0 +1,23 @@
+package ua.sjerra.book.numbers;
+
+/**
+ * Created by Natalya on 31.05.2015.
+ */
+public class StaticTest extends  StaticSuper{
+    static int rand;
+
+    static {
+        rand = (int) (Math.random()*6);
+        System.out.println("Static block " + rand);
+    }
+
+    StaticTest(){
+        System.out.println("StaticTest");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Inside main");
+        StaticTest ts = new StaticTest();
+
+    }
+}
