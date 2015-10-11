8ff39338ba7a1fdc008e1c3bf36c51f37859e7c8
maxura <maxura@ukr.net>
2015-10-12 00:22:58 +0300

    add new examples

--- my-app/src/ua/ua/sjerra/book/graphic/MiniMusicPlayer2.java
+++ my-app/src/ua/ua/sjerra/book/graphic/MiniMusicPlayer2.java
@@ -1,45 +1,37 @@
-/**
- * ****************************************************************************
- * Copyright (c) 2012-2015 Codenvy, S.A.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * <p/>
- * Contributors:
- * Codenvy, S.A. - initial API and implementation
- * *****************************************************************************
- */
 package ua.ua.sjerra.book.graphic;
 
 import javax.sound.midi.*;
 
 /**
- * //
- *
- * @author Musienko Maxim
+ * Created by Natalya on 11.10.2015.
  */
-public class MiniMusicPlayer1 {
-    public static void main(String[] args) {
+public class MiniMusicPlayer2 implements ControllerEventListener {
+    public void go() {
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
+            int[] eventsIwant = {127};
+            sequencer.addControllerEventListener(this, eventsIwant);
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
-            for (int i = 0; i < 61; i++) {
+
+            for (int i = 5; i < 60; i += 4) {
                 track.add(makeEvent(144, 1, i, 100, i));
+                track.add(makeEvent(176, 1, 127, 0, i));
                 track.add(makeEvent(128, 1, i, 100, i + 2));
             }
             sequencer.setSequence(seq);
             sequencer.setTempoInBPM(220);
             sequencer.start();
-
         } catch (Exception ex) {
-
 
         }
     }
 
+    @Override
+    public void controlChange(ShortMessage event) {
+        System.out.println("lya");
+    }
     public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
         MidiEvent event = null;
         try {
@@ -50,4 +42,5 @@ public class MiniMusicPlayer1 {
         }
         return event;
     }
+
 }
