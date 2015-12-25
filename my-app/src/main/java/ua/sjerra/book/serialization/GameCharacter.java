1c5bfe8d3c657b94c2e8a10299d13af753ac6c36
Maxim Musienko <mmusienko@codenvy.com>
2015-12-25 19:23:54 +0200

    add seraliz class

---
+++ my-app/src/main/java/ua/sjerra/book/serialization/GameCharacter.java
@@ -0,0 +1,49 @@
+/**
+ * ****************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * <p/>
+ * Contributors:
+ * Codenvy, S.A. - initial API and implementation
+ * *****************************************************************************
+ */
+package main.java.ua.sjerra.book.serialization;
+
+import java.io.Serializable;
+
+/**
+ * //
+ *
+ * @author Musienko Maxim
+ */
+public class GameCharacter implements Serializable {
+    int      power;
+    String   type;
+    String[] weapons;
+
+    public GameCharacter(int p, String t, String[] w) {
+        power = p;
+        type = t;
+        weapons = w;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    private String getWeapons() {
+        String weaponList = "";
+        for (int i = 0; i < weapons.length; i++) {
+            weaponList += weapons[i] + " ";
+
+        }
+        return weaponList;
+    }
+}
